00801dd354f407b6f55dc1a21cdde2d3fa5a7eca
Gordon Grandbouche <gjg54@georgetown.edu>
2024-12-22 13:21:04 -0500

    BuildDeck and Shuffle funcs, deck infra

--- hello.go
+++ hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	// "math"
 )
 
@@ -12,23 +13,41 @@ type Card struct {
 // Define the interface
 type DeckOpps interface {
 	BuildDeck();
+	Shuffle();
 }
 
 type Deck struct {
 	my_deck [52]Card
 }
 
+//function to initialize new deck of cards
 func (d *Deck) BuildDeck() {
+	//create every card, from val 0-12 and suit 0-3
 	for i := 0; i < len(d.my_deck); i++ {
 		d.my_deck[i] = Card{value: i%13, suit: i/13}
 	}
 }
 
+//function to shuffle a deck of cards into a random order
+func (d *Deck) Shuffle() {
+	// go's math/rand std lib comes with a shuffle funciton.
+	// it expects the number of items and a the function to perform a swap, 
+	// which is super easy in go thanks this wierd syntax
+	rand.Shuffle(
+		len(d.my_deck), // num items
+		func(i, j int) { //swap function
+			d.my_deck[i], d.my_deck[j] = d.my_deck[j], d.my_deck[i]
+		})
+}
+
 func main() {
-	var testingDeck DeckOpps 
-	deck := Deck{}
-	testingDeck = &deck
-	testingDeck.BuildDeck()
-	fmt.Println(deck.my_deck)
+	//init deck of cards
+	var newDeck = Deck{}
+	newDeck.BuildDeck()
+	fmt.Println("newDeck:", newDeck.my_deck)
+
+	//shuffle deck of cards
+	newDeck.Shuffle()
+	fmt.Println("\nnewDeck: ", newDeck)
 
 }
