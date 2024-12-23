1d002f32031583cf8ca5b962dbf184f66cebb645
Gordon Grandbouche <gjg54@georgetown.edu>
2024-12-23 15:13:26 -0500

    added shuffle

--- cmd/GameBoard/main.go
+++ cmd/GameBoard/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand/v2"
+)
 
 /*
 	Game board structures
@@ -31,59 +34,26 @@ func (board *GameBoard) IntitializeBoard() {
 	board.hand2 = make([]Card, 0, 11)
 	board.discard = make([]Card, 0, 52)
 
+}
 
+//function to shuffle a deck of cards into a random order
+func (board *GameBoard) Shuffle() {
+	// go's math/rand std lib comes with a shuffle funciton.
+	// it expects the number of items and a the function to perform a swap, 
+	// which is super easy in go thanks this wierd syntax
+	rand.Shuffle(
+		len(board.deck), // num items
+		func(i, j int) { //swap function
+			board.deck[i], board.deck[j] = board.deck[j], board.deck[i]
+		})
 }
 
 
 func main() {
-	//Test Deck
-	// //init deck of cards
-	// var newDeck = Deck{}
-	// newDeck.BuildDeck()
-
-	// //shuffle deck of cards
-	// newDeck.Shuffle()
-
-	// fmt.Println("\nnewDeck: ", newDeck)
-
-	// c := newDeck.DealCard()
-	// fmt.Print("\ntop card: ", c)
-	// fmt.Print("\nnewDeck after dealcard; ", newDeck, )
-	// fmt.Println("\nnewDeck.topCardIndex; ", newDeck.topCardIndex)
-
-	// //reset deck
-	// newDeck.BuildDeck()
-	// newDeck.Shuffle()
-
 	var board = GameBoard{}
 	board.IntitializeBoard()
 	fmt.Println(board)
-
-	
+	board.Shuffle()
+	fmt.Println(board)
 }
 
-
-
-// //function to shuffle a deck of cards into a random order
-// func (d *Deck) Shuffle() {
-// 	// go's math/rand std lib comes with a shuffle funciton.
-// 	// it expects the number of items and a the function to perform a swap, 
-// 	// which is super easy in go thanks this wierd syntax
-// 	rand.Shuffle(
-// 		len(d.my_deck), // num items
-// 		func(i, j int) { //swap function
-// 			d.my_deck[i], d.my_deck[j] = d.my_deck[j], d.my_deck[i]
-// 		})
-// }
-
-// func (d *Deck) DealCard() Card {
-// 	var topCard Card = d.my_deck[d.topCardIndex]
-// 	//take top card off of deck
-// 	d.my_deck[d.topCardIndex].suit = -1
-// 	d.my_deck[d.topCardIndex].value = -1
-
-// 	//update topCardIndex
-// 	d.topCardIndex--
-
-// 	return topCard
-// }
