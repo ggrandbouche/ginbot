3b85412fd8b0c491f9f4ab0906711cee3f5c4320
harris roberts <harrisaroberts@gmail.com>
2024-12-24 01:27:53 -0500

    Added straight finder

--- cmd/GameBoard/main.go
+++ cmd/GameBoard/main.go
@@ -33,10 +33,10 @@ func (board *GameBoard) IntitializeBoard() {
 
 }
 
-//function to shuffle a deck of cards into a random order
+// function to shuffle a deck of cards into a random order
 func (board *GameBoard) Shuffle() {
 	// go's math/rand std lib comes with a shuffle funciton.
-	// it expects the number of items and a the function to perform a swap, 
+	// it expects the number of items and a the function to perform a swap,
 	// which is super easy in go thanks this wierd syntax
 	rand.Shuffle(
 		len(board.deck), // num items
@@ -59,13 +59,13 @@ func (board *GameBoard) DealHands() {
 func printHand(hand []Card) {
 	value := []string{"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"}
 	suit := []string{"♠", "♦", "♣", "♥"}
-    var builder strings.Builder
+	var builder strings.Builder
 	for _, card := range hand {
 		builder.WriteString(fmt.Sprintf("%s%s, ", value[card.value], suit[card.suit]))
 	}
-    str := builder.String()
-    str = str[:len(str)-2]
-    fmt.Println(str)
+	str := builder.String()
+	str = str[:len(str)-2]
+	fmt.Println(str)
 }
 
 func (board *GameBoard) DealCard() Card {
@@ -74,16 +74,16 @@ func (board *GameBoard) DealCard() Card {
 	return card
 }
 
-func (board * GameBoard) SortHands() {
+func (board *GameBoard) SortHands() {
 	board.hand1 = sortHand(board.hand1)
 	board.hand2 = sortHand(board.hand2)
 }
 
 func sortHand(hand []Card) []Card {
-	for i := 0; i < len(hand) - 1; i++ {
+	for i := 0; i < len(hand)-1; i++ {
 		min := i
-		for j := i+1; j < len(hand); j++ {
-			if (hand[j].value + hand[j].suit*13 < hand[min].value + hand[min].suit*13){
+		for j := i + 1; j < len(hand); j++ {
+			if hand[j].value+hand[j].suit*13 < hand[min].value+hand[min].suit*13 {
 				min = j
 			}
 		}
@@ -96,16 +96,42 @@ func sortHand(hand []Card) []Card {
 	return hand
 }
 
+func groupify(hand []Card) int {
+    return 0
+}
+
+func findStraight(hand []Card, straight [][]Card) [][]Card{
+    for i := 0; i < len(hand) - 2; i++ {
+        if(hand[i].suit == hand[i+2].suit){
+            if(hand[i].value - hand[i+2].value == -2){
+                group := []Card{hand[i], hand[i+1], hand[i+2]} 
+                for j := i+3; j < len(hand); j++ {
+                    if(hand[j].suit == hand[i+2].suit){
+                        if(hand[j].value - hand[j-1].value == 1) {
+                            group = append(group, hand[j])
+                        }
+                    }
+                }
+                straight = append(straight, group)
+            }
+        }
+    }
+    return straight
+}
 
 func main() {
 	var board = GameBoard{}
 	board.IntitializeBoard()
 
-    board.Shuffle()
-    board.DealHands()
+	board.Shuffle()
+	board.DealHands()
 	board.SortHands()
-	
+
 	printHand(board.hand1)
 	printHand(board.hand2)
 
+    var straight [][]Card
+    straight = findStraight(board.hand1, straight)
+    fmt.Println(straight)
 }
+
