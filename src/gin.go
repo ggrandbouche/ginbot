3f5ca078f45fb69a33712ce761b044201f6c8f26
harrisaroberts <harrisaroberts@gmail.com>
2024-12-27 01:10:10 -0500

    ok

--- src/gin.go
+++ src/gin.go
@@ -22,8 +22,7 @@ func gameLoop(board *GameBoard) (bool, int){
             board.hand1 = append(board.hand1, board.discard[len(board.discard)])
             board.discard = board.discard[:len(board.discard)-1]
         }
-        fmt.Println(`Please enter the index of the card you would like to discard,
-            starting at index 0\n> `)
+        fmt.Println("Please enter the index of the card you would like to discard, starting at index 0\n> ")
         fmt.Scan(&input)
         board.discard = append(board.discard, board.hand1[input])
         board.hand1 = append(board.hand1[:input], board.hand1[input+1:]...)
