5ca9ed031497e043715da8e04226b2c78fb20c4e
harris roberts <harrisaroberts@gmail.com>
2024-12-30 14:18:04 -0500

    added comments

--- testing-servers/client.go
+++ testing-servers/client.go
@@ -8,18 +8,24 @@ import (
 )
 
 func main() {
+    // Creates a connection to the ec2 server with the public ip and the choosen
+    // port
     conn, err := net.Dial("tcp", "18.118.160.185:8080")
+    // if there is an error connecting we return and print the error
     if err != nil {
         fmt.Println("Error connecting to server:", err)
         return
     }
+    //  make sure the connection is closed once we return 
     defer conn.Close()
 
     fmt.Println("Connected to the server. Type your message:")
 
+    // scanner used to read user input from terminal
     scanner := bufio.NewScanner(os.Stdin)
     for scanner.Scan() {
         msg := scanner.Text() + "\n"
+        // send the message from use to the server, again logging errors 
         _, err := conn.Write([]byte(msg))
         if err != nil {
             fmt.Println("Error sending message:", err)
--- testing-servers/server.go
+++ testing-servers/server.go
@@ -6,39 +6,51 @@ import (
     "net"
 )
 
+// this func handles all the individual client connections 
 func handleConnection(conn net.Conn, messages chan string) {
+    // make sure connection is closed on return
     defer conn.Close()
+    
+    // create a reader for the connection
     reader := bufio.NewReader(conn)
 
     for {
+        // read messages from the client connection and log errors
         msg, err := reader.ReadString('\n')
         if err != nil {
             fmt.Println("Connection closed:", err)
             return
         }
 
+        // send the received message to the message channel
         messages <- msg
     }
 }
 
 func main() {
+    // start a server listening on port 8080
     listener, err := net.Listen("tcp", ":8080")
 
+    // log errors
     if err != nil {
         fmt.Println("Error starting server:", err)
         return
     }
+    // close listener on return
     defer listener.Close()
 
     fmt.Println("Server listening on port 8080")
 
+    // make the channel to handle client messages
     messages := make(chan string)
+    // go routine to print the messages received from clients
     go func() {
         for msg := range messages {
             fmt.Println("Message received:", msg)
         }
     }()
 
+    // accept connections or throw error
     for {
         conn, err := listener.Accept()
         
@@ -46,6 +58,7 @@ func main() {
             fmt.Println("Error accepting connection:", err)
             continue
         }
+        // handle each connection in its own go routine
         go handleConnection(conn, messages)
     }
 }
