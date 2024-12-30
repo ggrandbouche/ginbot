a0cbae202fc4d4f615e4f7e6d079dfa4bf8d2874
harris roberts <harrisaroberts@gmail.com>
2024-12-30 13:32:22 -0500

    testing some server stuff

---
+++ testing-servers/client.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "net"
+    "os"
+)
+
+func main() {
+    conn, err := net.Dial("tcp", "18.118.160.185:8080")
+    if err != nil {
+        fmt.Println("Error connecting to server:", err)
+        return
+    }
+    defer conn.Close()
+
+    fmt.Println("Connected to the server. Type your message:")
+
+    scanner := bufio.NewScanner(os.Stdin)
+    for scanner.Scan() {
+        msg := scanner.Text() + "\n"
+        _, err := conn.Write([]byte(msg))
+        if err != nil {
+            fmt.Println("Error sending message:", err)
+            return
+        }
+    }
+}
---
+++ testing-servers/server.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "net"
+)
+
+func handleConnection(conn net.Conn, messages chan string) {
+    defer conn.Close()
+    reader := bufio.NewReader(conn)
+
+    for {
+        msg, err := reader.ReadString('\n')
+        if err != nil {
+            fmt.Println("Connection closed:", err)
+            return
+        }
+
+        messages <- msg
+    }
+}
+
+func main() {
+    listener, err := net.Listen("tcp", ":8080")
+
+    if err != nil {
+        fmt.Println("Error starting server:", err)
+        return
+    }
+    defer listener.Close()
+
+    fmt.Println("Server listening on port 8080")
+
+    messages := make(chan string)
+    go func() {
+        for msg := range messages {
+            fmt.Println("Message received:", msg)
+        }
+    }()
+
+    for {
+        conn, err := listener.Accept()
+        
+        if err != nil {
+            fmt.Println("Error accepting connection:", err)
+            continue
+        }
+        go handleConnection(conn, messages)
+    }
+}
